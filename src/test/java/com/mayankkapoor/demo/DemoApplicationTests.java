94af30267377577bdc528457e45b2a28b245949b
Mayank Kapoor <mayankkapoormail@gmail.com>
2021-02-23 12:32:40 +0530

    Initial commit with hello world method and route

---
+++ src/test/java/com/mayankkapoor/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayankkapoor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
